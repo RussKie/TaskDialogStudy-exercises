8b6d297df99458e6a28030013d86b2e3873ac0a5
Igor Velikorossov <igor.velikorossov@microsoft.com>
2019-11-22 03:07:10 -0800

    Scenario #6
    Having completed the previous exercises it wasn't too bad, though
    few things felt confusing.
    
    Issues:
    * Unable to access the dialog in event handlers, which inhibits ability
    to create dialog pages outside the originating method without leaking
    the dialog instance into a class context.
    * "Created" event should be called "Shown" because that what it does
    from a consumer perspective.

--- TaskDialogStudy/Form1.cs
+++ TaskDialogStudy/Form1.cs
@@ -215,9 +215,126 @@ namespace TaskDialogStudy
 
             var taskDialog = new TaskDialog();
 
-            // write code here...
+            // [!] This feels unnecessary verbose - why can't we apply settings to the default/current page?
+            taskDialog.Page.MainInstruction = "Clean up database?";
+            taskDialog.Page.Text = "Do you really want to do a clean up?" + Environment.NewLine + "The action is irreversible!";
+            taskDialog.Page.Caption = "My Application";
+            taskDialog.Page.Icon = TaskDialogIcon.SecurityWarningYellowBar;
+            // [!] Inconsistent naming
+            taskDialog.Page.AllowCancel = true;         // [!] name is confusing
+            taskDialog.Page.CanBeMinimized = true;      // [?] akin to Form.MinimizeBox
 
-            taskDialog.ShowDialog(this);
+            // As noted above, this is confusing and overly verbose
+            TaskDialogStandardButton button1 = taskDialog.Page.StandardButtons.Add(TaskDialogResult.Yes);
+            TaskDialogStandardButton button2 = taskDialog.Page.StandardButtons.Add(TaskDialogResult.No);
+            button1.Enabled = false;
+            button1.ShouldCloseDialog = false;          // [?] Don't quite like the name
+            button2.DefaultButton = true;
+
+            var verificationText = new TaskDialogCheckBox("I know what I'm doing");
+            verificationText.CheckedChanged += (s, e) => button1.Enabled = verificationText.Checked;
+            taskDialog.Page.CheckBox = verificationText;
+
+            var page2 = new TaskDialogPage
+            {
+                AllowCancel = false,
+
+                MainInstruction = "Operation in progress...",
+                Text = "Please wait while we are deleting all your customers data...",
+                Icon = TaskDialogIcon.Information,
+                SizeToContent = true,
+
+                ProgressBar = new TaskDialogProgressBar(TaskDialogProgressBarState.Marquee),
+                Expander = new TaskDialogExpander()
+                {
+                    CollapsedButtonText = "See &details",
+                    ExpandedButtonText = "Hide &details",
+                    Text = "Initialising...",
+                    ExpandFooterArea = true             // [?] This name is confusing, had to run an example to find what it does
+                }
+            };
+
+            TaskDialogStandardButton button3 = page2.StandardButtons.Add(TaskDialogResult.Close);
+            button3.Enabled = false;
+            button3.ShouldCloseDialog = false;
+
+            var page3 = new TaskDialogPage
+            {
+                MainInstruction = "Success",
+                Text = "All your customers data is gone. Mwuahah!",
+                Icon = TaskDialogIcon.SecuritySuccessGreenBar,
+
+                CustomButtonStyle = TaskDialogCustomButtonStyle.CommandLinks
+            };
+
+            page3.CustomButtons.Add("Show &results");
+            page3.StandardButtons.Add(TaskDialogResult.Close);
+
+            // [!] Feels kind of leaky, perhaps it should pass the dialog in parameters
+            //      ... (s, e) => e.Dialog.Page = page2;
+            // Especially if a multipage dialog is assembled across multiple methods. 
+            // Right now it is impossible to write the following, without creating a field to hold the dialog instance:
+            //      button1.Click += button1_Clicked;
+            button1.Click += (s, e) => taskDialog.Page = page2;
+
+            // [!] This should be called "Shown", whilst "Created" is what is happening, from the consumer perspective - the page is shown.
+            page2.Created += Page2_Created;
+
+            button3.Click += (s, e) => taskDialog.Page = page3;
+
+            var result = taskDialog.ShowDialog(this);
+            Debug.WriteLine($"{MethodInfo.GetCurrentMethod().Name}: result: {result}");
+        }
+
+        private void Page2_Created(object sender, EventArgs e)
+        {
+            // [!] How do I get the instance of the dialog????
+            // [?] Alright I had to run under the debugger, and see that I can get the page from sender...
+            var page = sender as TaskDialogPage;
+            if (page == null)
+            {
+                return;
+            }
+
+            int progress = -30;
+            var timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += (s, e) =>
+            {
+                if (progress < 0)
+                {
+                    progress++;
+                    return;
+                }
+                if (progress == 0)
+                {
+                    page.ProgressBar.State = TaskDialogProgressBarState.Normal;
+                    page.ProgressBar.Minimum = 0;
+                    page.ProgressBar.Maximum = 100;
+                }
+
+                page.Expander.Text = $"Progress: {progress++} %";
+                page.ProgressBar.Value++;
+
+                if (progress > 100)
+                {
+                    timer.Stop();
+
+                    // [!] This feels awkward, I may want to create my next page here, e.g.
+                    //      taskDialog.Page = page3;
+                    // In the context of this example, something failed and I want to render an [Error] page,
+                    // or render a [Warning] page...
+                    page.StandardButtons[TaskDialogResult.Close].PerformClick();
+                    
+                }
+            };
+            timer.Start();
+
+            page.Destroyed += (s, e) =>
+            {
+                timer.Stop();
+            };
+
         }
     }
 }
